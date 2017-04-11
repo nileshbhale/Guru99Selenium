34bbb9fa9bd2ba2f6926813a02baef2c7c895dd4
nileshbhale <nilesh.bhale@gmail.com>
2017-04-11 10:55:43 +0530

    Program2 - Locate the elements by its ID

---
+++ selguru99/src/main/java/newproject/PG2.java
@@ -0,0 +1,22 @@
+package newproject;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class PG2 
+{
+
+	public static void main (String[] args)
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\nilesh.bhale\\git\\Guru99Selenium\\selguru99\\src\\main\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		String baseUrl = "https://gmail.com";
+		String tagName = "";
+		
+		driver.get(baseUrl);
+		tagName = driver.findElement(By.id("Email")).getTagName();
+		System.out.println(tagName);
+		driver.close();
+		System.exit(0);		
+	}
+}
